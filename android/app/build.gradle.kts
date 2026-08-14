854668f20a07d03146c31520205648ffac10d421
tvonbox3-byte <tvonbox3@gmail.com>
2026-08-13 23:59:35 -0300

    LPSM 2.2.10 ajustar HOME e espaço do banner

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         minSdk = 23
         targetSdk = 35
 
-        versionCode = 26
-        versionName = "2.2.9"
+        versionCode = 27
+        versionName = "2.2.10"
 
         buildConfigField(
             "String",
