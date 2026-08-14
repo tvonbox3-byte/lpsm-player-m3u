b26bf4cbdb12f66b2ee73b5f2fd3bcb7b0fab291
tvonbox3-byte <tvonbox3@gmail.com>
2026-08-14 13:56:29 -0300

    Preparar atualização LPSM 2.2.15

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         minSdk = 23
         targetSdk = 35
 
-        versionCode = 31
-        versionName = "2.2.14"
+        versionCode = 32
+        versionName = "2.2.15"
 
         buildConfigField(
             "String",
