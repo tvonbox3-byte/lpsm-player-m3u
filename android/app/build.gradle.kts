c49c76fb8e05c65811764b7d6a907738c62a4b88
lpsmservice <tvonbox24@gmail.com>
2026-08-22 05:02:35 -0300

    LPSM 2.2.28 - contagens e imagens em rede fraca

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         minSdk = 21
         targetSdk = 35
 
-        versionCode = 50
-        versionName = "2.2.27"
+        versionCode = 51
+        versionName = "2.2.28"
 
         buildConfigField(
             "String",
